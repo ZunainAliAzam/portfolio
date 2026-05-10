98d2b6a8967c31443f847c5ff1d878c14dae721e
ZunainAliAzam <zunainazam456@gmail.com>
2026-05-10 22:51:04 +0500

    fix: sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://muhammadamas.dev',
+  site: 'https://zunainaliazam.vercel.app',
   integrations: [
     tailwind({ applyBaseStyles: false }),
     mdx(),
